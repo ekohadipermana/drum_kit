def7cf38339294ad39d760e96ea563a2f412d299
ekohadipermana <ekohadipermana@gmail.com>
2024-01-30 11:17:15 +0700

    creating for loop and commenting out previous lines

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
-alert('Hello there');
-document.querySelector('button').addEventListener('click', gotClick); // this line to check if the button clicked then will call gotClick function
+// alert('Hello there');
+// document.querySelector('button').addEventListener('click', gotClick);  this line to check if the button clicked then will call gotClick function
+
+// document.querySelector('.set button').addEventListener('click', gotClick);
 
 function gotClick(){
     alert('I got clicked');
 }
+
+// new one
+for (let i = 0; i < document.querySelectorAll('.drum').length; i++){
+    document.querySelectorAll('button')[i].addEventListener('click', gotClick);
+}
+

9cfb073b093983ccc532ee1d2e48b5850fdef073
ekohadipermana <ekohadipermana@gmail.com>
2024-01-30 09:15:05 +0700

    creating add event listener to check button clicked

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 alert('Hello there');
+document.querySelector('button').addEventListener('click', gotClick); // this line to check if the button clicked then will call gotClick function
+
+function gotClick(){
+    alert('I got clicked');
+}

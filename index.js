d78172e869c67ddfc981bd54b2d07ac0d3f0344c
ekohadipermana <ekohadipermana@gmail.com>
2024-02-20 21:27:59 +0700

    creating elements

--- index.js
+++ index.js
@@ -3,15 +3,22 @@
 
 // document.querySelector('.set button').addEventListener('click', gotClick);
 
+// creating elements
+let buttonW = document.querySelector('.w');
+let buttonS = document.querySelector('.s');
+let buttonA = document.querySelector('.a');
 
-function gotClick(){
+
+function gotClick() {
     // alert('I got clicked');
     let audio = new Audio('sounds/crash.mp3');
     audio.play();
-
+}
 
 
 // new one
 for (let i = 0; i < document.querySelectorAll('.drum').length; i++){
     document.querySelectorAll('button')[i].addEventListener('click', gotClick);
 }
+
+
